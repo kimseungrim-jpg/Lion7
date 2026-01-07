acabf71d8fab7745686c7b71484048469428c8e7
kimseungrim-jpg <kimseungrim@gmail.com>
2026-01-07 16:50:25 +0900

    Day04_commit

---
+++ Day04_02/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Day04_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("생성할 몬스터의 수를 입력해주세요: ");
+            //int monsterNum = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < monsterNum; i++)
+            //{
+            //    if (i == 0)
+            //    {
+            //        Console.WriteLine("=== 몬스터 웨이브 시작 ===");
+            //        Console.WriteLine($"고블린 #{i + 1} 생성!");
+            //    }
+            //    else if(i > 0 && i < monsterNum - 1)
+            //    {
+            //        Console.WriteLine($"고블린 #{i + 1} 생성!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"고블린 #{i + 1} 생성!");
+            //        Console.WriteLine($"총 {i + 1}마리 생성 완료!");
+            //    }
+            //}
+
+            //Console.Write("카운트 다운을 설정: ");
+            //int cnt = int.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine("=== 게임 시작 카운트다운 ===");
+            //for (int i = cnt; i > 0; i--)
+            //{
+            //    Console.WriteLine($"{i}...");
+            //}
+            //Console.WriteLine("게임 시작!");
+
+            //string sword = "무한의 대검";
+
+            //Console.WriteLine("당신은 20번뽑기가 가능합니다. 지금 실행합니다.");
+
+            //Random rand = new Random();
+
+            //int random = 0;
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    random = rand.Next(1, 101);
+
+            //    if (random >= 1 && random <= 10)
+            //    {
+            //        sword = "무한의 대검";
+            //    }
+            //    else if (random >= 11 && random <= 30)
+            //    {
+            //        sword = "카타나";
+            //    }
+            //    else if (random >= 31 && random <= 60)
+            //    {
+            //        sword = "엑스칼리버";
+            //    }
+            //    else if (random >= 61 && random <= 100)
+            //    {
+            //        sword = "정기정검";
+            //    }
+
+            //    Console.WriteLine(sword);
+            //    Thread.Sleep(500);
+            //}
+
+            Random rnd = new Random();
+
+            string level = "rare"; //시작등급 레어
+            int cube = 0; // 레어 업 확률 10% 에픽 업 확률 5% 유니크 업 확률 1%
+            int cnt = 0; // 큐브를 돌리면서 시행한 횟수
+            int totalCnt = 0;
+
+            while (true)
+            {
+                Console.WriteLine($"현재 등급: {level}");
+                Console.WriteLine($"등급 시행 횟수: {cnt}/ 전체 시행 횟수: {totalCnt}");
+                Console.Write("시행하시겠습니까?(y/n/c):"); //y n c 값중 받아서 반복문 진행
+                string check = Console.ReadLine(); // check값 받음
+
+                if (check == "n") //n을 입력받으면 반복문 탈출
+                {
+                    Console.WriteLine("다음에 또 이용해 주세요~!!!(●'ㅅ'●)");
+                    break;
+                }
+                else if (check == "y") //y를 입력받으면 시행
+                {
+                    cnt++;
+                    totalCnt++;
+                    cube = rnd.Next(1, 101); //시행시 카운트가 증가하고 랜덤값 부여
+                    //Console.WriteLine(cube);
+                    if (level == "rare") //큐브의 등급에 따라 등급될 확률을 다르게 설정 후 진행
+                    {
+                        if (cube > 0 && cube <= 10) //cube가 10보다 아래라면 즉 10프로라면 업
+                        {
+                            level = "epic";
+                            Console.WriteLine($"★★★축하합니다. 등급이 올랐습니다.★★★");
+                            Console.WriteLine($"승급한 등급: {level}");
+                            Console.WriteLine("____________________________________________");
+                            cnt = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("실패했습니다. 다시 한번 시도해주세요.");
+                            Console.WriteLine("____________________________________________");
+                        }
+                    }
+                    else if (level == "epic")
+                    {
+                        if (cube > 0 && cube <= 5)
+                        {
+                            level = "unique";
+                            Console.WriteLine($"★★★축하합니다. 등급이 올랐습니다.★★★");
+                            Console.WriteLine($"승급한 등급: {level}");
+                            Console.WriteLine("____________________________________________");
+                            cnt = 0;
+                        }
+                        else
+                        {
+                            Console.WriteLine("실패했습니다. 다시 한번 시도해주세요.");
+                            Console.WriteLine("____________________________________________");
+                        }
+                    }
+                    else if (level == "unique")
+                    {
+                        if (cube > 0 && cube <= 1)
+                        {
+                            level = "legendary";
+                            Console.WriteLine("★★★최종등급 달성을 축하드립니다!!★★★");
+                            Console.WriteLine("=== 최종 LEGENDARY 등급 달성★ ===");
+                            Console.WriteLine($"최종 시행 횟수: {cnt}");
+
+                            Console.WriteLine("____________________________________________");
+                            Console.Write("다시 시도 하시겠습니까??:(y/n) ");
+                            check = Console.ReadLine(); //최종 등급 달성시 다시 할거냐는 문구출력을 해서 입력받아 진행
+
+                            if (check == "y")
+                            {
+                                level = "rare";
+                                cnt = 0;
+                                totalCnt = 0;
+                                Console.Clear(); //다시 진행할시 출력문과 카운트, 등급을 초기화 해서 처음부터 진행
+                            }
+                            else
+                            {
+                                Console.WriteLine("다음에 또 이용해 주세요~!!!(●'ㅅ'●)");
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("실패했습니다. 다시 한번 시도해주세요.");
+                            Console.WriteLine("____________________________________________");
+                        }
+                    }
+
+                }
+                else if(check == "c") //c입력시 출력문 초기화
+                {
+                    Console.Clear();
+                }
+                else // y,n,c 외 다른 문자 입력시 문구 출력 후 반복
+                {
+                    Console.WriteLine("잘못입력하셨습니다.");
+                    Console.WriteLine("____________________________________________");
+                }
+            }
+        }
+    }
+}
---
+++ Day4_01/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int level = 45;
+            //string result = (level > 50) ? "고급" : (level <= 50 && level >= 30) ? "중급" : "초급";
+
+            //Console.WriteLine("플레이어등급");
+            //Console.WriteLine($"레벨: {level}");
+            //Console.WriteLine($"등급: {result}");
+
+            //int health = 30;
+            //int maxhealth = 100;
+
+            //string result = (health > 70) ? "안전" : (health >= 30) ? "주의" : "위험";
+
+            //Console.WriteLine("===플레이어 상태===");
+            //Console.WriteLine($"체력: {health}/{maxhealth}");
+            //Console.WriteLine($"상태: {result}");
+
+            //int baseDamage = 50;
+            //int bonusDamage = 20;
+            //double criticalMultiplier = 1.5;
+
+            //// 잘못된 계산
+            //double damage1 = baseDamage + bonusDamage * criticalMultiplier;
+            //// 올바른 계산
+            //double damage2 = (baseDamage + bonusDamage) * criticalMultiplier;
+
+            //Console.WriteLine("\n=== 크리티컬 데미지 계산 ===");
+            //Console.WriteLine($"기본 데미지: {baseDamage}");
+            //Console.WriteLine($"보너스 데미지: {bonusDamage}");
+            //Console.WriteLine($"크리티컬 배율: {criticalMultiplier}");
+            //Console.WriteLine($"잘못된 계산: {damage1}");  // 80.0
+            //Console.WriteLine($"올바른 계산: {damage2}");  // 105.0
+
+            //double health = 30.0;
+            //double maxHealth = 100.0;
+
+            //double healthPer = (health / maxHealth) * 100;
+            //Console.WriteLine($"현재 체력: {health}/{maxHealth}");
+
+            //if (healthPer <=30 && healthPer > 0)
+            //{
+            //    Console.WriteLine("경고: 체력이 위험합니다!");
+            //    Console.WriteLine("체력 아이템을 사용하세요!");
+            //    Console.WriteLine("체력이 50% 이하입니다.");
+            //}
+            //else if (healthPer > 30 && healthPer <= 50)
+            //{
+            //    Console.WriteLine( "체력이 50% 이하입니다.");
+            //}
+            //else if(healthPer == 0)
+            //{
+            //    Console.WriteLine("게임 오버!");
+            //    Console.WriteLine("부활 지점에서 다시 시작합니다.");
+            //}
+
+            //Console.WriteLine();
+
+            //int enemyDistance = 5;
+            //int attackRange = 3;
+
+            //if (attackRange <= enemyDistance)
+            //{
+            //    Console.WriteLine("적이 사거리 안에 있습니다.");
+            //    Console.WriteLine("공격 가능!");
+            //}
+
+            //Console.OutputEncoding = Encoding.UTF8;
+
+            //Console.WriteLine("=== 캐릭터 상태 ===");
+            //int health = int.Parse(Console.ReadLine());
+
+            //if (health >= 80)
+            //{
+            //    Console.WriteLine("💚 상태: 매우 좋음");
+            //}
+            //else if (health >= 60)
+            //{
+            //    Console.WriteLine("🟢 상태: 좋음");
+            //}
+            //else if (health >= 40)
+            //{
+            //    Console.WriteLine("🟡 상태: 보통");
+            //}
+            //else if (health >= 20)
+            //{
+            //    Console.WriteLine("🟠 상태: 위험");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("🔴 상태: 매우 위험!");
+            //}
+
+            Console.WriteLine("=== 캐릭터 직업을 고르시오===");
+            Console.WriteLine("1. 전사       | 2. 궁수       | 3.마법사");
+            Console.WriteLine("클래스: 전사  | 클래스: 궁수  | 클래스: 마법사");
+            Console.WriteLine("공격력: 3     | 공격력: 4     | 공격력: 5");
+            Console.WriteLine("방어력: 5     | 방어력: 2     | 방어력: 1");
+            Console.WriteLine("스피드: 2     | 스피드: 4     | 스피드: 1");
+            Console.WriteLine("사거리: 1     | 사거리: 5     | 사거리: 4");
+
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("=== 캐릭터 생성 완료 ===");
+                    Console.WriteLine("클래스: 전사");
+                    break;
+                case 2:
+                    Console.WriteLine("=== 캐릭터 생성 완료 ===");
+                    Console.WriteLine("클래스: 궁수");
+                    break;
+                case 3:
+                    Console.WriteLine("=== 캐릭터 생성 완료 ===");
+                    Console.WriteLine("클래스: 마법사");
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
