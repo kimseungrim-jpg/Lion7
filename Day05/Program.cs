d6ce46c5c298204a7fc6d7c516dc9107d57bb1ce
kimseungrim-jpg <kimseungrim@gmail.com>
2026-01-08 17:04:40 +0900

    Day05

---
+++ Day05/Program.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Day05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 10;
+
+            //while (i > 0)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+            //int coins = 0;
+            //int target = 50;
+            //int day = 0;
+
+            //while (coins <= target)
+            //{
+            //    coins += 10;
+            //    day++;
+            //    Console.WriteLine($"{day}일차: 코인 {coins}개");
+            //}
+
+            //Console.WriteLine($"목표 달성: {day}일 걸렸습니다.");
+
+            //for (int i = 1; i <= 10; i++)
+            // {
+            //     if (i % 2 == 0)
+            //     {
+            //         continue;
+            //     }
+            //     Console.WriteLine(i);
+            // }
+
+            //Console.WriteLine("=== 대장장이 키우기 ===");
+
+            //Random rand = new Random();
+
+            //Console.WriteLine("대장장이 키우기");
+
+            //int pmoney = 100;
+            //int input;
+            //int rnd;
+
+            //Thread.Sleep(500);
+
+            //while (true)
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine("1. 나무캐기 ");
+            //    Console.WriteLine("2. 장비뽑기 ");
+            //    Console.WriteLine("3. 나가기 ");
+            //    Console.Write("입력: ");
+            //    input = int.Parse(Console.ReadLine());
+
+            //    if (input == 1)
+            //    {
+            //        while (true)
+            //        {
+            //            Console.Write("나무캐기(엔터)");
+            //            Console.WriteLine("뒤로가기 x");
+
+            //            string str = Console.ReadLine();
+
+            //            pmoney += 100;
+            //            Console.WriteLine("소지금 : " + pmoney);
+            //            if (str == "x")
+            //            {
+            //                Console.WriteLine("뒤로가기");
+            //                break;
+            //            }
+
+            //        }
+            //    }
+
+            //}
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        Console.Write($"({j}, {i})");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    for (int j = 0; j < 5; j++)
+            //    {
+            //        if (j >= i)
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        else
+            //        {
+            //            Console.Write(" ");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 1; i <= 3; i++)
+            //{
+            //    for (int j = 1; j <= 3; j++)
+            //    {
+            //        Console.Write($"{i}x{j}={i * j} ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    for (int j = 0; j < 4; j++)
+            //    {
+            //        if (i == 0 && j == 0)
+            //        {
+            //            Console.Write("☞ ");
+            //        }
+            //        else if (i == 3 && j==3) 
+            //        {
+            //            Console.Write("＠");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("■ ");
+            //        }
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            const int targetX = 50;
+            const int targetY = 20;
+
+            int x = 10;
+            int y = 10;
+
+            ConsoleKeyInfo keyInfo;
+            
+            while (true)
+            {
+                Console.Clear();
+
+                Console.SetCursorPosition(x, y);
+
+                Console.Write("●");
+
+                Console.SetCursorPosition(targetX, targetY);
+                Console.Write("♨");
+
+                keyInfo = Console.ReadKey(true);
+
+                if (x == targetX && y == targetY)
+                {
+                    Console.Clear();
+                    Random rand = new Random();
+
+                    Console.WriteLine(" 대장장이 키우기 ");
+
+                    int pmoney = 100;
+                    int input;
+                    int rnd;
+
+                    Thread.Sleep(500);
+
+
+                    //무한반복
+                    while (true)
+                    {
+                        Console.Clear(); //화면지우기
+                        Console.WriteLine("1. 나무캐기 ");
+                        Console.WriteLine("2. 장비뽑기 ");
+                        Console.WriteLine("3. 나가기 ");
+                        Console.Write("입력 : ");
+                        input = int.Parse(Console.ReadLine()); //input에 키로 눌린 숫자 담기
+
+                        if (input == 1) //나무캐기화면
+                        {
+                            while (true)
+                            {
+                                Console.WriteLine("나무캐기(엔터)");
+                                Console.WriteLine("뒤로가기 x");
+
+                                string str = Console.ReadLine();
+
+                                pmoney += 100;
+                                Console.WriteLine("소지금 : " + pmoney);
+                                if (str == "x")
+                                {
+                                    Console.WriteLine("뒤로가기");
+                                    break;
+                                }
+
+
+                            }
+                        }
+                        else if (input == 2)
+                        {
+                            //장비뽑기
+                            if (pmoney >= 1000) //돈이 있는지 확인후 뽑기
+                            {
+                                pmoney -= 1000;
+
+                                //20번 뽑기
+                                for (int i = 1; i <= 20; i++)
+                                {
+                                    rnd = rand.Next(1, 101);
+
+                                    if (rnd == 1) // 1퍼
+                                    {
+                                        Console.WriteLine("도끼등급 SSS");
+                                    }
+                                    else if (rnd >= 2 && rnd <= 6)
+                                    {
+                                        Console.WriteLine("도끼등급 SS");
+                                    }
+                                    else if (rnd >= 7 && rnd <= 17)
+                                    {
+                                        Console.WriteLine("도끼등급 S");
+                                    }
+                                    else if (rnd >= 18 && rnd <= 38)
+                                    {
+                                        Console.WriteLine("도끼등급 A");
+                                    }
+                                    else if (rnd >= 39 && rnd <= 69)
+                                    {
+                                        Console.WriteLine("도끼등급 B");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("도끼등급 C");
+                                    }
+                                    Thread.Sleep(500); //0.5초정도로 뽑혀라.
+
+                                }
+
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("돈이 부족합니다. \n");
+                                Thread.Sleep(1000);
+                            }
+                        }
+                        else if (input == 3)
+                        {
+                            Console.WriteLine("게임을 나갑니다.");
+                            Environment.Exit(0);
+                        }
+
+                    }
+                }
+            
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (y > 0)
+                        {
+                            y--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (y < Console.WindowHeight - 1)
+                        {
+                            y++;
+                        }
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (x < Console.WindowWidth - 1)
+                        {
+                            x++;
+                        }
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (x > 0)
+                        {
+                            x--;
+                        }
+                        break;
+                    case ConsoleKey.Spacebar:
+                        Console.Write("미사일키");
+                        break;
+                    case ConsoleKey.Escape:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Day05_02/Program.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day05_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int shopX = 20;
+            const int shopY = 10;
+            const int mineX = 50;
+            const int mineY = 10;
+            const int guildX = 35;
+            const int guildY = 5;
+            const int resxX = 35;
+            const int restY = 15;
+
+            int x = 35;
+            int y = 10;
+            string level = "초급 광부";
+            int money = 5000;
+            int health = 100;
+            int maxhealth = 100;
+            int levelPoint = 0;
+
+            ConsoleKeyInfo keyInfo;
+
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine($"등급 : {level}/{levelPoint}");
+                Console.WriteLine($"소지금 : {money}");
+                Console.WriteLine($"체력 : {health}/{maxhealth}");
+               
+                Console.SetCursorPosition(x, y);
+                Console.Write("§");
+
+                Console.SetCursorPosition(shopX - 3, shopY - 1);
+                Console.Write("㈜상점㈜");
+                Console.SetCursorPosition(shopX, shopY);
+                Console.Write("○");
+
+                Console.SetCursorPosition(mineX - 3, mineY - 1);
+                Console.Write("↑광산↑");
+                Console.SetCursorPosition(mineX, mineY);
+                Console.Write("○");
+
+                Console.SetCursorPosition(guildX - 3, guildY - 1);
+                Console.Write("※길드※");
+                Console.SetCursorPosition(guildX, guildY);
+                Console.Write("○");
+
+                Console.SetCursorPosition(resxX - 4, restY + 1);
+                Console.Write("♨휴식터♨");
+                Console.SetCursorPosition(resxX, restY);
+                Console.Write("○");
+
+                Console.SetCursorPosition(0, restY + 2);
+                Console.Write("________________________________________________________________________________");
+
+                Random rand = new Random();
+
+                if (x == shopX && y == shopY)
+                {
+                    Console.SetCursorPosition(0, restY + 4);
+                    Console.WriteLine("재련하기 위해 필요한 골드 요구량 ↓");
+                    Console.WriteLine("초급 광부 : 500골드 / 중급 광부 : 1500골드 / 고급 광부 : 3000골드");
+                    
+                    if (level == "초급 광부")
+                    {
+                        if (money >= 500 && levelPoint != 100)
+                        {
+                            Console.SetCursorPosition(0, restY + 8);
+                            Console.WriteLine("승급 점수 10점 상승!!!!");
+                            levelPoint += 10;
+                            money -= 500;
+                            Console.WriteLine($"남은 승급 점수 {100 - levelPoint}점");
+                        }
+                        else if (money < 500)
+                        {
+                            Console.SetCursorPosition(0, restY + 8);
+                            Console.WriteLine("골드가 부족합니다! 골드를 모아오십시오.");
+                        }
+                        else if (levelPoint == 100)
+                        {
+                            Console.SetCursorPosition(0, restY + 8);
+                            Console.WriteLine("이미 승급 점수가 충분합니다! 길드로 가서 승급하십시오!");
+                        }
+                    }
+                    else if (level == "중급 광부")
+                    {
+                        if (money >= 1500 && levelPoint != 100)
+                        {
+                            Console.SetCursorPosition(0, restY + 8);
+                            Console.WriteLine("승급 점수 10점 상승!!!!");
+                            levelPoint += 10;
+                            money -= 500;
+                            Console.WriteLine($"남은 승급 점수 {100 - levelPoint}점");
+                        }
+                        else if (money < 500)
+                        {
+                            Console.SetCursorPosition(0, restY + 8);
+                            Console.WriteLine("골드가 부족합니다! 골드를 모아오십시오.");
+                        }
+                        else if (levelPoint == 100)
+                        {
+                            Console.SetCursorPosition(0, restY + 8);
+                            Console.WriteLine("이미 승급 점수가 충분합니다! 길드로 가서 승급하십시오!");
+                        }
+                    }
+                    else if (level == "고급 광부")
+                    {
+                        if (money >= 3000 && levelPoint != 100)
+                        {
+                            Console.SetCursorPosition(0, restY + 8);
+                            Console.WriteLine("승급 점수 10점 상승!!!!");
+                            levelPoint += 10;
+                            money -= 500;
+                            Console.WriteLine($"남은 승급 점수 {100 - levelPoint}점");
+                        }
+                        else if (money < 500)
+                        {
+                            Console.SetCursorPosition(0, restY + 8);
+                            Console.WriteLine("골드가 부족합니다! 골드를 모아오십시오.");
+                        }
+                        else if (levelPoint == 100)
+                        {
+                            Console.SetCursorPosition(0, restY + 8);
+                            Console.WriteLine("이미 승급 점수가 충분합니다! 길드로 가서 승급하십시오!");
+                        }
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(0, restY + 8);
+                        Console.WriteLine("전문가는 승급 포인트가 오르지 않습니다.");
+                    }
+                    x++;
+                }
+                else if (x == mineX && y == mineY)
+                {
+                    if (health > 30)
+                    {
+                        int rnd = rand.Next(1, 101);
+                        if (level == "초급 광부")
+                        {
+                            if (rnd == 1)
+                            {
+                                money += 2000;
+                                health -= 30;
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("다이아몬드 발견!★");
+                                Console.WriteLine("소지금 2000골드 흭득!");
+                            }
+                            else if (rnd > 1 && rnd <= 60)
+                            {
+                                money += 50;
+                                health -= 30;
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("동 발견...");
+                                Console.WriteLine("소지금 50골드 흭득!");
+                            }
+                            else if (rnd > 60 && rnd <= 90)
+                            {
+                                money += 150;
+                                health -= 30;
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("은 발견");
+                                Console.WriteLine("소지금 150골드 흭득!");
+                            }
+                            else if (rnd > 90 && rnd <= 100)
+                            {
+                                money += 2000;
+                                health -= 30;
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("금 발견!");
+                                Console.WriteLine("소지금 400골드 흭득!");
+                            }
+                        }
+                        else if (level == "중급 광부")
+                        {
+                            if (rnd > 0 && rnd <= 3)
+                            {
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("다이아몬드 발견!★");
+                                Console.WriteLine("소지금 2000골드 흭득!");
+                                money += 2000;
+                                health -= 30;
+                            }
+                            else if (rnd > 3 && rnd <= 50)
+                            {
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("동 발견...");
+                                Console.WriteLine("소지금 50골드 흭득!");
+                                money += 50;
+                                health -= 30;
+                            }
+                            else if (rnd > 50 && rnd <= 85)
+                            {
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("은 발견");
+                                Console.WriteLine("소지금 150골드 흭득!");
+                                money += 150;
+                                health -= 30;
+                            }
+                            else if (rnd > 85 && rnd <= 100)
+                            {
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("금 발견!");
+                                Console.WriteLine("소지금 400골드 흭득!");
+                                money += 2000;
+                                health -= 30;
+                            }
+                        }
+                        else if (level == "고급 광부")
+                        {
+                            if (rnd > 0 && rnd <= 5)
+                            {
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("다이아몬드 발견!★");
+                                Console.WriteLine("소지금 2000골드 흭득!");
+                                money += 2000;
+                                health -= 30;
+                            }
+                            else if (rnd > 1 && rnd <= 40)
+                            {
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("동 발견...");
+                                Console.WriteLine("소지금 50골드 흭득!");
+                                money += 50;
+                                health -= 30;
+                            }
+                            else if (rnd > 40 && rnd <= 80)
+                            {
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("은 발견");
+                                Console.WriteLine("소지금 150골드 흭득!");
+                                money += 150;
+                                health -= 30;
+                            }
+                            else if (rnd > 80 && rnd <= 100)
+                            {
+                                Console.SetCursorPosition(0, restY + 4);
+                                Console.WriteLine("금 발견!");
+                                Console.WriteLine("소지금 400골드 흭득!");
+                                money += 2000;
+                                health -= 30;
+                            }
+                        }
+                        else if (level == "전문가")
+                        {
+                            Console.SetCursorPosition(0, restY + 4);
+                            Console.WriteLine("다이아몬드 발견!★");
+                            Console.WriteLine("소지금 2000골드 흭득!");
+                            money += 2000;
+                            health -= 30;
+                        }
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(0, restY + 4);
+                        Console.WriteLine("체력이 부족하여 광산에 들어갈 수 없습니다!");
+                        Console.WriteLine("체력을 회복해 주세요!");
+                    }
+                    x--;
+                }
+                else if (x == guildX && y == guildY)
+                {
+                    if (levelPoint == 100)
+                    {
+                        if (level == "초급 광부")
+                        {
+                            level = "중급 광부";
+                            Console.SetCursorPosition(0, restY + 4);
+                            Console.WriteLine("축하합니다 중급 광부로 승급하셨습니다!");
+                            levelPoint = 0;
+                        }
+                        else if (level == "중급 광부")
+                        {
+                            level = "고급 광부";
+                            Console.SetCursorPosition(0, restY + 4);
+                            Console.WriteLine("축하합니다 고급 광부로 승급하셨습니다!");
+                            levelPoint = 0;
+                        }
+                        else if(level == "고급 광부")
+                        {
+                            level = "전문가";
+                            Console.SetCursorPosition(0, restY + 4);
+                            Console.WriteLine("축하합니다 고급 광부로 승급하셨습니다!");
+                            levelPoint = 0;
+                        }
+                        else
+                        {
+                            Console.SetCursorPosition(0, restY + 4);
+                            Console.WriteLine("죄송합니다. 이미 최고 등급의 광부입니다.");
+                        }
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(0, restY + 4);
+                        Console.WriteLine("승급에 필요한 점수가 모자릅니다!");
+                    }
+                        y++;
+                }
+                else if(x == resxX && y == restY)
+                {
+                    if (health != maxhealth)
+                    {
+                        health = maxhealth;
+                        Console.SetCursorPosition(0, restY + 4);
+                        Console.WriteLine("휴식을 취해 체력을 가득 채웠습니다!");
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(0, restY + 4);
+                        Console.WriteLine("체력이 이미 가득 차있습니다!");
+                    }
+                    y--;
+                }
+
+                keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow: if (y > 0) y--; break;
+                    case ConsoleKey.DownArrow: if (y < Console.WindowHeight - 1) y++; break;
+                    case ConsoleKey.LeftArrow: if (x > 0) x--; break;
+                    case ConsoleKey.RightArrow: if (x < Console.WindowWidth - 1) x++; break;
+                    case ConsoleKey.Escape: break;
+
+                }
+
+            }
+
+        }
+    }
+}
