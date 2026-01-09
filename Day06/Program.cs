d7996322c869b39c22ff1f3e16534b009e9b7080
kimseungrim-jpg <kimseungrim@gmail.com>
2026-01-09 17:11:58 +0900

    Day06

---
+++ Day06/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    string[] inventory = new string[5];
+
+            //    Console.WriteLine("=== 인벤토리 ===");
+
+            //    for (int i = 0; i < inventory.Length; i++)
+            //    {
+            //        inventory[i] = Console.ReadLine();
+            //    }
+            //    for (int i = 0; i < inventory.Length; i++)
+            //    {
+            //        Console.WriteLine($"[{i + 1}] {inventory[i]}");
+            //    }
+
+            //int[] character = new int[5];
+            //string[] state = { "HP", "MP", "공격력", "방어력", "민첩" };
+
+            //for (int i = 0; i < character.Length; i++)
+            //{
+            //    Console.Write($"스탯 입력 [{state[i]}]:");
+            //    character[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("=== 캐릭터 스텟===");
+            //for (int i = 0; i < character.Length; i++)
+            //{
+            //    Console.WriteLine($"{state[i]}: {character[i]}");
+            //}
+
+            //string[] monsterName = { "고블린", "오크", "슬라임", "드래곤", "좀비" };
+            //int[] recentNum = { 5, 3, 8, 2, 7 };
+            //int[] maxNum = { 5, 5, 5, 5, 5 };
+
+            //for (int i = 0; i < monsterName.Length; i++)
+            //{
+            //    Console.Write($"{monsterName[i]}: {recentNum[i]} / {maxNum[i]}");
+            //    if (recentNum[i] >= maxNum[i])
+            //    {
+            //        Console.WriteLine("● 완료");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("○ 진행중");
+            //    }
+            //}
+
+            //int[] scores = { 85, 92, 78, 95, 88 };
+
+            ////배열 길이
+            //Console.WriteLine("총 점수 개수: " + scores.Length);
+
+            ////배열 순회
+            //Console.WriteLine("개별 점수");
+            //for (int i = 0; i < scores.Length; i++)
+            //{
+            //    Console.WriteLine($"플레이어 {i + 1} : {scores[i]}점");
+            //}
+
+
+            ////합계 계산
+            //int sum = 0;
+
+            //for (int i = 0; i < scores.Length; i++)
+            //{
+            //    sum += scores[i];
+            //}
+
+            //Console.WriteLine($"총점 : {sum}점");
+            //Console.WriteLine($"평균 : {(float)sum / (float)scores.Length}점");
+
+            //int max = scores[0];
+            //int min = scores[0];
+
+            //for (int i = 1; i < scores.Length; i++)
+            //{
+            //    if (max < scores[i])
+            //    {
+            //        max = scores[i];
+            //    }
+            //    if (min > scores[i])
+            //    {
+            //        min = scores[i];
+            //    }
+            //}
+
+            //Console.WriteLine($"최고점: {max} / 최저점: {min}");
+
+            //string[,] number = { { "A1", "A2", "A3" }, { "B1", "B2", "B3" }, { "C1", "C2", "C3" } };
+
+            //for (int i = 0; i < number.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < number.GetLength(1); j++)
+            //    {
+            //        Console.Write($"[{number[i, j]}]");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine($"첫 번째 좌석: {number[0, 0]}");
+
+           //Dictionary<string, int> item = new Dictionary<string, int>();
+
+           // item.Add("회복 포션", 50);
+           // item.Add("마나 포션", 40);
+           // item.Add("강철 검", 500);
+           // item.Add("가죽 갑옷", 300);
+           // item.Add("마법 반지", 1000);
+
+           // Console.WriteLine("=== 상점 아이템 ===");
+           // foreach (KeyValuePair<string, int> sell in item)
+           // {
+           //     Console.WriteLine($"{sell.Key}: {sell.Value}");
+           // }
+
+           // string searchItem = "강철 검";
+           // Console.WriteLine();
+
+           // if (item.ContainsKey(searchItem))
+           // {
+           //     Console.WriteLine($"'{searchItem}'구매 성공!");
+           //     Console.WriteLine("남은 골드: 100");
+           // }
+           // else
+           // {
+           //     Console.WriteLine("해당 아이템이 존재하지 않습니다.");
+           // }
+
+
+        }
+    }
+}
---
+++ Day06_02/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day06_02
+{
+    internal class Program
+    {
+        [DllImport("msvcrt.dll")]
+        static extern int _getch(); //c언어 함수 가져옴
+
+        static void Main(string[] args)
+        {
+            string[] player = new string[]
+            {
+                "->",
+                ">>>",
+                "->"
+            };
+
+            string atk = "->";
+
+            int playerX = 0;
+            int playerY = 12;
+
+            int atkX = playerX + 3;
+            int atkY = playerY + 1;
+
+            List<(int,int)> atkList = new List<(int,int )>();
+
+            //ConsoleKeyInfo keyInfo;
+
+            Console.CursorVisible = false; // 콘솔창 커서 지우기
+
+            int dwTime = Environment.TickCount; // 1/1000 초 단위로 초가 흐름
+
+
+
+            while (true)
+            {
+                //1초루프
+                if (dwTime + 50 < Environment.TickCount)
+                {
+                    //현재 시간 세팅
+                    dwTime = Environment.TickCount;
+
+                    Console.Clear();
+
+                    //키영역
+                    int pressKey;
+
+                    if (Console.KeyAvailable)
+                    {
+                        pressKey = _getch();
+
+                        if (pressKey == 224) 
+                        {
+                            pressKey = _getch();
+                        }
+
+                        switch (pressKey)
+                        {
+                            case 72:  //위쪽방향 아스키코드                    
+                                playerY--;
+                                if (playerY < 1)
+                                    playerY = 1;
+                                break;
+                            case 75:
+                                //왼쪽 화살표키
+                                playerX--;
+                                if (playerX < 0)
+                                    playerX = 0;
+                                break;
+                            case 77:
+                                //오른쪽
+                                playerX++;
+                                if (playerX > 75)
+                                    playerX = 75;
+                                break;
+                            case 80: //아래
+                                playerY++;
+                                if (playerY > 21)
+                                    playerY = 21;
+                                break;
+                            case 32:
+                                atkList.Add((playerX + 3, playerY + 1));
+                            break;
+                        }
+                    }
+
+                    for (int i = 0; i < player.Length; i++)
+                    {
+                        //콘솔좌표 설정 플레이어X 플레이어Y
+                        Console.SetCursorPosition(playerX, playerY + i);
+                        //문자열배열 출력
+                        Console.WriteLine(player[i]);
+                    }
+
+                    if (atkList.Count > 0)
+                    {
+                        for (int i = 0; i < atkList.Count; i++)
+                        {
+                            Console.SetCursorPosition(atkList[i].Item1, atkList[i].Item2);
+                            Console.WriteLine("->");
+
+                            if (atkList[i].Item1 > 75)
+                            {
+                                atkList.Remove(atkList[i]);
+                            }
+                            else
+                            {
+                                atkList[i] = (atkList[i].Item1 + 1, atkList[i].Item2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
